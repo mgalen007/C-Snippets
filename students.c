4e5f13e65c5763224e5024e7dcd3bc637d1fcd3f
RUGWIRO Mike Galen <hyllusx@gmail.com>
2026-03-17 14:37:25 +0200

    Add basic program with structs

---
+++ students.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+// Define a Student struct
+struct Student {
+    int id, age;
+    char lname[20], fname[20], gender[6];
+};
+
+// Declare functions to read and show properties
+void setProperties(struct Student *stud, int id, int age, char fname[20], char lname[20], char gender[6]) {
+    (*stud).id = id;
+    (*stud).age = age;
+    strcpy((*stud).fname, fname);
+    strcpy((*stud).lname, lname);
+    strcpy((*stud).gender, gender);
+}
+void getProperties(struct Student *stud) {
+    printf("\nID: %d", (*stud).id);
+    printf("\nAge: %d", (*stud).age);
+    printf("\nFirst name: %s", (*stud).fname);
+    printf("\nLast name: %s", (*stud).lname);
+    printf("\nGender: %s", (*stud).gender);
+}
+
+int main() {
+    // Declare two students
+    struct Student stud1, stud2;
+
+    // Set their properties
+    setProperties(&stud1, 1, 16, "Rugwiro", "Galen", "male");
+    setProperties(&stud2, 2, 15, "Atete", "Eunice", "female");
+
+    // Print the properties
+    getProperties(&stud1);
+    getProperties(&stud2);
+}
